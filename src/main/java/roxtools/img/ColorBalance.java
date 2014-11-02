1b1c93a6644853bd3c6136ae2ad3c84304823a73
gracilianomp <gracilianomp@gmail.com>
2014-11-02 00:37:37 -0200

    add minScaleRatio, to avoid balance of almost plain images

--- src/main/java/roxtools/img/ColorBalance.java
+++ src/main/java/roxtools/img/ColorBalance.java
@@ -10,6 +10,10 @@ import roxtools.RichConsole;
 
 public class ColorBalance {
 	
+	static final public float BALANCE_COLORS_S1_DEFAULT = 1.50f ;
+	static final public float BALANCE_COLORS_S2_DEFAULT = 1.50f ;
+	static final public float BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT = 0.10f ;
+	
 	static private int[] toInts(float[] fs, int scale) {
 		int[] ns = new int[fs.length] ;
 		
@@ -21,18 +25,22 @@ public class ColorBalance {
 	}
 	
 	static public void balanceColors(float[] pixelsChannel) {
-		balanceColors(pixelsChannel, 1.5f, 1.5f) ;
+		balanceColors(pixelsChannel, BALANCE_COLORS_S1_DEFAULT, BALANCE_COLORS_S2_DEFAULT, BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT) ;
 	}
 	
 	static public void balanceColors(byte[] pixelsChannel) {
-		balanceColors(pixelsChannel, 1.5f, 1.5f) ;
+		balanceColors(pixelsChannel, BALANCE_COLORS_S1_DEFAULT, BALANCE_COLORS_S2_DEFAULT, BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT) ;
 	}
 	
 	static public void balanceColors(int[] pixelsChannel) {
-		balanceColors(pixelsChannel, 1.5f, 1.5f) ;
+		balanceColors(pixelsChannel, BALANCE_COLORS_S1_DEFAULT, BALANCE_COLORS_S2_DEFAULT, BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT) ;
 	}
 	
 	static public void balanceColors(float[] pixelsChannel, float s1, float s2) {
+		balanceColors(pixelsChannel, s1, s2, BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT);
+	}
+	
+	static public void balanceColors(float[] pixelsChannel, float s1, float s2, float minScaleRatio) {
 		int[] c1 = toInts(pixelsChannel, 255) ;
 		
 		for (int i = 0; i < c1.length; i++) {
@@ -41,7 +49,7 @@ public class ColorBalance {
 			else if (v > 255) c1[i] = 255 ;
 		}
 		
-		balanceColors(c1, s1, s2) ;
+		balanceColors(c1, s1, s2, minScaleRatio) ;
 		
 		for (int i = pixelsChannel.length-1 ; i >= 0 ; i--) {
 			pixelsChannel[i] = c1[i] / 255f ;
@@ -49,8 +57,12 @@ public class ColorBalance {
 	}
 	
 	static public void balanceColors(byte[] pixelsChannel, float s1, float s2) {
+		balanceColors(pixelsChannel, s1, s2, BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT);
+	}
+	
+	static public void balanceColors(byte[] pixelsChannel, float s1, float s2, float minScaleRatio) {
 		int[] c1 = ImagePixels.toInts(pixelsChannel) ;
-		balanceColors(c1, s1, s2) ;
+		balanceColors(c1, s1, s2, minScaleRatio) ;
 		
 		for (int i = pixelsChannel.length-1 ; i >= 0 ; i--) {
 			pixelsChannel[i] = (byte) c1[i] ;
@@ -58,6 +70,10 @@ public class ColorBalance {
 	}
 	
 	static public void balanceColors(int[] pixelsChannel, float s1, float s2) {
+		balanceColors(pixelsChannel, s1, s2, BALANCE_COLORS_MIN_SCALE_RATIO_DEFAULT);
+	}
+	
+	static public void balanceColors(int[] pixelsChannel, float s1, float s2, float minScaleRatio) {
 		
 		int n = pixelsChannel.length ;
 		
@@ -88,6 +104,15 @@ public class ColorBalance {
 
 		if (vMax < 255-1) vMax++ ;
 		
+		int vScale = vMax-vMin ; 
+		
+		int minScale = (int) (255 * minScaleRatio) ;
+		
+		//System.out.println("color balance>> "+ s1+" ; "+s2 +" > "+ vMin +" .. "+ vMax +" > "+ vScale +" / "+ minScale );
+		
+		
+		if (vScale <= minScale) return ;
+		
 		for (int i = 0; i < n; i++) {
 			int p = pixelsChannel[i] ;
 			if ( p < vMin ) {
@@ -98,8 +123,6 @@ public class ColorBalance {
 			}
 		}
 		
-		int vScale = vMax-vMin ;
-		
 		for (int i = 0; i < n; i++) {
 			int p = pixelsChannel[i] ;
 			
@@ -108,7 +131,7 @@ public class ColorBalance {
 		
 	}
 
-	static public void testGray(File file, float s1, float s2) throws IOException {
+	static public void testGray(File file, float s1, float s2, float minScaleRatio) throws IOException {
 		BufferedImage img = ImageIO.read(file) ;
 		
 		ImagePixels imagePixels = new ImagePixels(img) ;
@@ -119,14 +142,14 @@ public class ColorBalance {
 		ImagePixels imagePixels1 = new ImagePixels( c1, imagePixels.getWidth(), imagePixels.getHeight() , imagePixels.isYUVFormat() ) ;
 		RichConsole.print( imagePixels1.createImage() ).configureMaxHeight(300) ;
 		
-		balanceColors(c1, s1, s2) ;
+		balanceColors(c1, s1, s2, minScaleRatio) ;
 		
 		ImagePixels imagePixels3 = new ImagePixels( c1, imagePixels.getWidth(), imagePixels.getHeight() , imagePixels.isYUVFormat() ) ;
 	
 		RichConsole.printLn( imagePixels3.createImage() ).configureMaxHeight(300) ;
 	}
 	
-	static public void testRGB(File file, float s1, float s2) throws IOException {
+	static public void testRGB(File file, float s1, float s2, float minScaleRatio) throws IOException {
 
 		BufferedImage img = ImageIO.read(file) ;
 		
@@ -138,9 +161,9 @@ public class ColorBalance {
 		byte[] c2 = imagePixels.getPixelsC2() ;
 		byte[] c3 = imagePixels.getPixelsC3() ;
 		
-		balanceColors(c1, s1, s2) ;
-		balanceColors(c2, s1, s2) ;
-		balanceColors(c3, s1, s2) ;
+		balanceColors(c1, s1, s2, minScaleRatio) ;
+		balanceColors(c2, s1, s2, minScaleRatio) ;
+		balanceColors(c3, s1, s2, minScaleRatio) ;
 		
 		ImagePixels imagePixels2 = new ImagePixels(c1, c2, c3, imagePixels.getWidth(), imagePixels.getHeight(), imagePixels.isYUVFormat()) ;
 		
@@ -152,13 +175,14 @@ public class ColorBalance {
 		
 		float s1 = 1.5f ;
 		float s2 = 1.5f ;
+		float minScaleRatio = 0.10f ;
 		
 		for (int i = 0; i < 12; i++) {
 			File file = new File("/Volumes/SAFEZONE/tmp/img-color-balance-test"+i+".png") ;
 			System.out.println(file.length() +"> "+file);
 			
-			testRGB(file, s1, s2) ;
-			testGray(file, s1, s2) ;
+			testRGB(file, s1, s2, minScaleRatio) ;
+			testGray(file, s1, s2, minScaleRatio) ;
 		}
 		
 		
