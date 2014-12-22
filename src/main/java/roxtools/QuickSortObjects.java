3adb6b07ed9065193d3c93fb6d3b6f95274cc0dd
gracilianomp <gracilianomp@gmail.com>
2014-12-22 19:46:00 -0200

    simple quick sort for Objects

---
+++ src/main/java/roxtools/QuickSortObjects.java
@@ -0,0 +1,250 @@
+package roxtools;
+
+
+final public class QuickSortObjects {
+	
+	static public interface ObjectCompareValueInt<O> {
+		public int getObjectCompareValue(O obj) ;
+	}
+
+	static public interface ObjectCompareValueFloat<O> {
+		public float getObjectCompareValue(O obj) ;
+	}
+	
+	static public interface ObjectCompareValueDouble<O> {
+		public double getObjectCompareValue(O obj) ;
+	}
+	
+	////////////////////////////
+	
+	static public <O> void sort(O[] o, ObjectCompareValueInt<O> objCompareValue) {
+		int[] a = new int[o.length] ;
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = objCompareValue.getObjectCompareValue( o[i] ) ;
+		}
+		
+		sort(a, o);
+	}
+	
+	static public <O> void sort(O[] o, ObjectCompareValueFloat<O> objCompareValue) {
+		float[] a = new float[o.length] ;
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = objCompareValue.getObjectCompareValue( o[i] ) ;
+		}
+		
+		sort(a, o);
+	}
+	
+	static public <O> void sort(O[] o, ObjectCompareValueDouble<O> objCompareValue) {
+		double[] a = new double[o.length] ;
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i] = objCompareValue.getObjectCompareValue( o[i] ) ;
+		}
+		
+		sort(a, o);
+	}
+
+	////////////////////////////
+	
+	static public void sort(int[] a, Object[] o) {
+		quickSort(a, o, 0, a.length-1);
+	}
+	
+	static public void sort(float[] a, Object[] o) {
+		quickSort(a, o, 0, a.length-1);
+	}
+	
+	static public void sort(double[] a, Object[] o){
+		quickSort(a, o, 0, a.length-1);
+	}
+	
+	////////////////////////////
+	
+	static private void quickSort(int[] a, Object[] o, int left,int right){
+		if (left >= right) return;
+		 
+		int pivot = getMedian(a,o, left, right);
+		int partition = partition(a, o, left, right, pivot);
+		
+		quickSort(a, o, left, partition-1);
+		quickSort(a, o, partition+1, right);
+	}
+	
+	static private void quickSort(float[] a, Object[] o, int left,int right){
+		if (left >= right) return;
+		 
+		float pivot = getMedian(a,o, left, right);
+		int partition = partition(a, o, left, right, pivot);
+		
+		quickSort(a, o, left, partition-1);
+		quickSort(a, o, partition+1, right);
+	}
+	
+	static private void quickSort(double[] a, Object[] o, int left,int right){
+		if (left >= right) return;
+		 
+		double pivot = getMedian(a,o, left, right);
+		int partition = partition(a, o, left, right, pivot);
+		
+		quickSort(a, o, left, partition-1);
+		quickSort(a, o, partition+1, right);
+	}
+	
+	//////////////////////////
+	
+	public static int getMedian(int[]a, Object[] o, int left,int right){
+		int center = (left+right)/2;
+		
+		if(a[left] > a[center])
+			swap(a,o, left,center);
+		
+		if(a[left] > a[right])
+			swap(a,o, left, right);
+		
+		if(a[center] > a[right])
+			swap(a,o, center, right);
+		
+		// put median at pivot position:
+		swap(a,o, center, right);
+		
+		return a[right];
+	}
+	
+	public static float getMedian(float[]a, Object[] o, int left,int right){
+		int center = (left+right)/2;
+		
+		if(a[left] > a[center])
+			swap(a,o, left,center);
+		
+		if(a[left] > a[right])
+			swap(a,o, left, right);
+		
+		if(a[center] > a[right])
+			swap(a,o, center, right);
+		
+		// put median at pivot position:
+		swap(a,o, center, right);
+		
+		return a[right];
+	}
+	
+	public static double getMedian(double[]a, Object[] o, int left,int right){
+		int center = (left+right)/2;
+		
+		if(a[left] > a[center])
+			swap(a,o, left,center);
+		
+		if(a[left] > a[right])
+			swap(a,o, left, right);
+		
+		if(a[center] > a[right])
+			swap(a,o, center, right);
+		
+		// put median at pivot position:
+		swap(a,o, center, right);
+		
+		return a[right];
+	}
+	
+	//////////////////////////
+	
+	static private int partition(int[] a, Object[] o, int left,int right,int pivot){
+		int leftCursor = left-1;
+		int rightCursor = right;
+		
+		while(leftCursor < rightCursor){
+            while(a[++leftCursor] < pivot);
+            while(rightCursor > 0 && a[--rightCursor] > pivot);
+            
+			if (leftCursor >= rightCursor) {
+				break;
+			}
+			else {
+				swap(a, o, leftCursor, rightCursor);
+			}
+		}
+		
+		swap(a, o, leftCursor, right);
+		
+		return leftCursor;
+	}
+	
+	static private int partition(float[] a, Object[] o, int left,int right,float pivot){
+		int leftCursor = left-1;
+		int rightCursor = right;
+		
+		while(leftCursor < rightCursor){
+            while(a[++leftCursor] < pivot);
+            while(rightCursor > 0 && a[--rightCursor] > pivot);
+            
+			if (leftCursor >= rightCursor) {
+				break;
+			}
+			else {
+				swap(a, o, leftCursor, rightCursor);
+			}
+		}
+		
+		swap(a, o, leftCursor, right);
+		
+		return leftCursor;
+	}
+	
+	static private int partition(double[] a, Object[] o, int left,int right,double pivot){
+		int leftCursor = left-1;
+		int rightCursor = right;
+		
+		while(leftCursor < rightCursor){
+            while(a[++leftCursor] < pivot);
+            while(rightCursor > 0 && a[--rightCursor] > pivot);
+            
+			if (leftCursor >= rightCursor) {
+				break;
+			}
+			else {
+				swap(a, o, leftCursor, rightCursor);
+			}
+		}
+		
+		swap(a, o, leftCursor, right);
+		
+		return leftCursor;
+	}
+	
+	//////////////////////////
+	
+	static private void swap(int[] a, Object[] o, int idx1,int idx2){
+		int tmp = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = tmp;
+		
+		Object tmpO = o[idx1];
+		o[idx1] = o[idx2];
+		o[idx2] = tmpO;
+	}
+	
+	static private void swap(float[] a, Object[] o, int idx1,int idx2){
+		float tmp = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = tmp;
+		
+		Object tmpO = o[idx1];
+		o[idx1] = o[idx2];
+		o[idx2] = tmpO;
+	}
+	
+	static private void swap(double[] a, Object[] o, int idx1,int idx2){
+		double tmp = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = tmp;
+		
+		Object tmpO = o[idx1];
+		o[idx1] = o[idx2];
+		o[idx2] = tmpO;
+	}
+	
+}
+
---
+++ src/test/java/roxtools/QuickSortObjectsTest.java
@@ -0,0 +1,109 @@
+package roxtools;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.Test;
+
+import roxtools.QuickSortObjects.ObjectCompareValueInt;
+
+public class QuickSortObjectsTest {
+
+	static private Object[] createIntArray(int size, long seed){
+		Random rand = new Random(seed) ;
+		
+		int[] array = new int[size];
+		Object[] objs = new Object[size] ;
+		
+		for(int i=0;i<size;i++){
+			int v = rand.nextInt(size*10) ;
+			array[i] = v ;
+			objs[i] = "s:"+ v ;
+		}
+		
+		return new Object[] { array , objs } ;
+	}
+	
+	static private String[] createStringArray(int size, long seed){
+		Random rand = new Random(seed) ;
+		
+		String[] objs = new String[size] ;
+		
+		for(int i=0;i<size;i++){
+			int v = rand.nextInt(size*10) ;
+			objs[i] = ""+v ;
+		}
+		
+		return objs ;
+	}
+
+	static private void checkSortInt(int[] a, Object[] o) {
+		for (int i = 1; i < a.length; i++) {
+			int v = a[i];
+			int vPrev = a[i-1];
+			
+			if (vPrev > v) throw new IllegalStateException("Array not sorted") ;
+			
+			Object vO = o[i] ;
+			
+			if ( !("s:"+v).equals(vO) ) throw new IllegalStateException("Object not matching index") ;
+		}
+		
+	}
+	
+	static private void checkSortString(String[] o) {
+		for (int i = 1; i < o.length; i++) {
+			String strVal = o[i];
+			String strValPrev = o[i-1];
+			
+			int v = Integer.parseInt(strVal) ;
+			int vPrev = Integer.parseInt(strValPrev) ;
+			
+			if (vPrev > v) throw new IllegalStateException("Array not sorted") ;
+		}
+		
+	}
+
+	@Test
+	public void testSort() {
+		
+		long seed = 1859301237985L ^ -458203475L ^ 45631581085001L ;
+		
+		for (int loop = 0; loop < 10; loop++) {
+			Object[] ret = createIntArray(10000, seed) ;
+			
+			int[] a = (int[]) ret[0] ;
+			Object[] o = (Object[]) ret[1] ;
+			
+			QuickSortObjects.sort(a, o);
+			
+			checkSortInt(a, o);
+			
+			seed = seed ^ (seed * 31 + Arrays.hashCode(a)) ;
+		}
+		
+	}
+	
+	@Test
+	public void testSortObjects() {
+		
+		long seed = 1859301237985L ^ -458203475L ^ 45631581085001L ;
+		
+		for (int loop = 0; loop < 10; loop++) {
+			String[] o = createStringArray(10000, seed) ;
+			
+			QuickSortObjects.sort(o, new ObjectCompareValueInt<String>() {
+				@Override
+				public int getObjectCompareValue(String obj) {
+					return Integer.parseInt(obj) ;
+				}
+			});
+			
+			checkSortString(o);
+			
+			seed = seed ^ (seed * 31 + Arrays.hashCode(o)) ;
+		}
+		
+	}
+	
+}
