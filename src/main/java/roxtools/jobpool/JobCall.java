f2997befd5ac6eac6d4d0386aa079009c984fcb3
gracilianomp <gracilianomp@gmail.com>
2015-05-12 00:30:34 -0300

    optmize call

--- src/main/java/roxtools/jobpool/JobCall.java
+++ src/main/java/roxtools/jobpool/JobCall.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-final class JobCall implements Serializable {
+final public class JobCall implements Serializable , Runnable {
 	private static final long serialVersionUID = -5375821922993700225L;
 	
 	final private Object obj;
@@ -15,7 +15,7 @@ final class JobCall implements Serializable {
 	final private JobResult result ;
 	
 	@SuppressWarnings("rawtypes")
-	public JobCall(Object obj, Method method, Object[] args) {
+	protected JobCall(Object obj, Method method, Object[] args) {
 		this.obj = obj;
 		this.method = method;
 		this.args = args;
@@ -38,16 +38,35 @@ final class JobCall implements Serializable {
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void setResult(Object result) {
-		
 		if (result != null && result.getClass() == JobResult.class) {
 			result = ((JobResult)result).getResult() ;
 		}
 		
 		this.result.setResult(result);
 	}
+	
+	public void setError(Throwable error) {
+		result.setError(error);
+	}
 
 	public Object invokeLocal() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		return method.invoke(obj, args) ;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			Object res = invokeLocal() ;
+			setResult(res);
+		}
+		catch (IllegalAccessException | IllegalArgumentException e) {
+			e.printStackTrace() ;
+			setError(e);
+		}
+		catch (InvocationTargetException e) {
+			e.printStackTrace() ;
+			setError(e);
+		}	
 	}
 	
 }
--- src/main/java/roxtools/jobpool/JobPoolExecuterLocal.java
+++ src/main/java/roxtools/jobpool/JobPoolExecuterLocal.java
@@ -1,6 +1,5 @@
 package roxtools.jobpool;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ExecutorService;
 
 import roxtools.threadpool.RoxThreadPool;
@@ -17,39 +16,12 @@ public class JobPoolExecuterLocal implements JobPoolExecutor {
 		this.threadPool = threadPool;
 	}
 	
-	final static private class Task implements Runnable {
-		final private JobCall call ;
-		
-		public Task(JobCall call) {
-			this.call = call;
-		}
-		
-		@Override
-		public void run() {
-			try {
-				Object res = call.invokeLocal() ;
-				call.setResult(res);
-			}
-			catch (IllegalAccessException | IllegalArgumentException e) {
-				e.printStackTrace() ;
-				call.setResult(e);
-			}
-			catch (InvocationTargetException e) {
-				e.printStackTrace() ;
-				call.setResult(e.getCause());
-			}
-		}
-		
-	}
-	
 	@Override
 	public void executeJobs(JobCall[] calls) {
 		
 		for (int i = 0; i < calls.length; i++) {
 			JobCall call = calls[i];
-			
-			Task task = new Task(call) ;
-			threadPool.execute(task);
+			threadPool.execute(call);
 		}
 	
 	}
