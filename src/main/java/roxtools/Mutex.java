2d16510ee8e36ba3454dca58b977d0b5efcfc8e3
gracilianomp <gracilianomp@gmail.com>
2015-05-21 01:23:51 -0300

    A Serializable MUTEX

---
+++ src/main/java/roxtools/Mutex.java
@@ -0,0 +1,13 @@
+package roxtools;
+
+import java.io.Serializable;
+
+final public class Mutex implements Serializable {
+	private static final long serialVersionUID = -5220750241434721351L;
+
+	public Mutex() {
+		
+	}
+	
+
+}
