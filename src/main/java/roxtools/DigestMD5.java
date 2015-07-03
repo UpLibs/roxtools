459c95491d5ae0c23f34356542ff18d92812b910
gracilianomp <gracilianomp@gmail.com>
2015-07-02 23:35:52 -0300

    add calcMD5 for File and InputStream

--- src/main/java/roxtools/DigestMD5.java
+++ src/main/java/roxtools/DigestMD5.java
@@ -1,5 +1,9 @@
 package roxtools;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -31,6 +35,33 @@ final public class DigestMD5 {
 			digestMD5.reset() ;
 			return digest ;
 		}
+	}
+	
+	public byte[] calcMD5(File file) throws IOException {
+		FileInputStream fin = new FileInputStream(file) ;
+		
+		try {
+			return calcMD5(fin) ;			
+		}
+		finally {
+			fin.close();
+		}
+	}
+	
+	public byte[] calcMD5(InputStream in) throws IOException {
+		byte[] buff = new byte[1024*8] ;
+		int r ;
+		
+		synchronized (digestMD5) {
+			while ( (r = in.read(buff)) >= 0 ) {
+				digestMD5.update(buff, 0, r);
+			}
+			
+			byte[] digest = digestMD5.digest() ;
+			digestMD5.reset() ;
+			return digest ;
+		}
+		
 	}
 	
 }
