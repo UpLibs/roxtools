d2a6058f8882379a2b14933abbf673246a8d73a4
jorge <jorge@uppoints.com>
2016-06-17 19:07:02 -0300

    added more options to simple http client. upgraded wrapper to 2.14

--- src/main/java/roxtools/SimpleHTTPClient.java
+++ src/main/java/roxtools/SimpleHTTPClient.java
@@ -8,44 +8,54 @@ import java.net.URLConnection;
 
 public class SimpleHTTPClient {
 
-	static public String getAsString(URL url) throws IOException {
+    private static final int DEFAULT_TIMEOUT = 60000;
+
+    // Hides the implicit public constructor
+    private SimpleHTTPClient() {
+
+    }
+
+    static public String getAsString(URL url) throws IOException {
 		return getAsString(url, null) ;
 	}
-	
+
 	static public String getAsString(URL url, String userAgent) throws IOException {
-		byte[] data = get(url, userAgent) ;
-		return new String(data) ;
+        return getAsString(url, null, DEFAULT_TIMEOUT , DEFAULT_TIMEOUT) ;
 	}
-	
-	static public byte[] get(URL url) throws IOException {
-		return get(url, null) ;
+
+    static public String getAsString(URL url, String userAgent, int connectTimeout, int readTimeout) throws IOException {
+        byte[] data = get(url, userAgent, connectTimeout , readTimeout) ;
+        return new String(data) ;
+    }
+
+    static public byte[] get(URL url) throws IOException {
+		return get(url, null, DEFAULT_TIMEOUT, DEFAULT_TIMEOUT) ;
 	}
-	
-	static public byte[] get(URL url, String userAgent) throws IOException {
+
+    static public byte[] get(URL url, String userAgent) throws IOException {
+        return get( url, userAgent, DEFAULT_TIMEOUT , DEFAULT_TIMEOUT);
+    }
+
+	static public byte[] get( URL url, String userAgent, int connectTimeout, int readTimeout ) throws IOException {
 		URLConnection con = url.openConnection();
-		
-		con.setConnectTimeout(60000);
-		con.setReadTimeout(60000);
-		
+
+		con.setConnectTimeout(connectTimeout);
+		con.setReadTimeout(readTimeout);
+
 		if (userAgent != null && !userAgent.isEmpty()) {
 			con.setRequestProperty("User-Agent", userAgent) ;
 		}
-		
-		InputStream in = con.getInputStream() ;
-		
-		ByteArrayOutputStream bout = new ByteArrayOutputStream() ;
-		
-		byte[] buffer = new byte[1024 * 4];
-		int r = 0;
-		while ( (r = in.read(buffer)) >= 0 ) {
-			bout.write(buffer, 0, r);
-		}
-		
-		try {
-			in.close();
-		} catch (Exception e) {}
 
-		return bout.toByteArray() ;
-	}
+        ByteArrayOutputStream bout = new ByteArrayOutputStream() ;
+        try ( InputStream in = con.getInputStream() ) {
+            byte[] buffer = new byte[1024 * 4];
+            int r = 0;
+            while (( r = in.read( buffer )) >= 0 ) {
+                bout.write( buffer, 0, r );
+            }
+        }
+
+        return bout.toByteArray();
+    }
 
 }
